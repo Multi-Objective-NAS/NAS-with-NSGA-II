e0a8722bd9187dee8b995d49111f3c15e455ac82
Minyoung Choe <chocindy05@gmail.com>
2019-12-04 12:14:59 +0900

    improve readability

---
+++ constants.py
@@ -0,0 +1,17 @@
+from nasbench import api
+
+# Use nasbench_full.tfrecord for full dataset (run download command above).
+nasbench = api.NASBench('nasbench_only108.tfrecord')
+
+# Useful constants
+INPUT = 'input'
+OUTPUT = 'output'
+CONV3X3 = 'conv3x3-bn-relu'
+CONV1X1 = 'conv1x1-bn-relu'
+MAXPOOL3X3 = 'maxpool3x3'
+NUM_VERTICES = 7
+MAX_EDGES = 9
+EDGE_SPOTS = NUM_VERTICES * (NUM_VERTICES - 1) / 2   # Upper triangular matrix
+OP_SPOTS = NUM_VERTICES - 2   # Input/output vertices are fixed
+ALLOWED_OPS = [CONV3X3, CONV1X1, MAXPOOL3X3]
+ALLOWED_EDGES = [0, 1]   # Binary adjacency matrix
--- nsga2.py
+++ nsga2.py
@@ -1,46 +1,41 @@
-import setup as su
+import constants
+
+import numpy as np
+import random
+
 
 def random_spec():
-  """Returns a random valid spec."""
-  while True:
-    matrix = su.np.random.choice(su.ALLOWED_EDGES, size=(su.NUM_VERTICES, su.NUM_VERTICES))
-    matrix = su.np.triu(matrix, 1)
-    ops = su.np.random.choice(su.ALLOWED_OPS, size=(su.NUM_VERTICES)).tolist()
-    ops[0] = su.INPUT
-    ops[-1] = su.OUTPUT
-    spec = su.api.ModelSpec(matrix=matrix, ops=ops)
-    if su.nasbench.is_valid(spec):
-      return spec
+    """Returns a random valid spec."""
+    while True:
+        matrix = np.random.choice(constants.ALLOWED_EDGES, size=(constants.NUM_VERTICES, constants.NUM_VERTICES))
+        matrix = np.triu(matrix, 1)
+        ops = np.random.choice(constants.ALLOWED_OPS, size=constants.NUM_VERTICES).tolist()
+        ops[0] = constants.INPUT
+        ops[-1] = constants.OUTPUT
+        spec = constants.api.ModelSpec(matrix=matrix, ops=ops)
+        if constants.nasbench.is_valid(spec):
+            return spec
+
+
+def n_ary_to_m_ary(list_of_int, length, n, m):
+    decimal = int(''.join(map(str, list_of_int)), base=n)
+
+    # convert into m-ary list
+    m_ary = []
+    for _ in range(length):
+        decimal, leftover = divmod(decimal, m)
+        m_ary.append(leftover)
+
+    return m_ary[::-1]
+
 
 def binary_to_ternary(binary):
-    decimal = 0
-    length = len(binary)
+    return n_ary_to_m_ary(binary, length=5, n=2, m=3)
 
-    # fine decimal
-    for i in range(length):
-        decimal *= 2
-        decimal += binary[i]
-
-    #convert into ternary
-    ternary = []
-    for i in range(5):
-        ternary.insert(0, decimal % 3)
-        decimal = decimal //3
-
-    return ternary
 
 def ternary_to_binary(ternary):
-    decimal = 0
-    for i in range(5):
-        decimal *= 3
-        decimal += ternary[i]
+    return n_ary_to_m_ary(ternary, length=8, n=3, m=2)
 
-    binary = []
-    for i in range(8):
-        binary.insert(0, decimal % 2)
-        decimal = decimal // 2
-
-    return binary
 
 def encode(spec):
     #
@@ -48,99 +43,104 @@ def encode(spec):
     ternary_code = []
 
     for r in range(7):
-        for c in range(r+1,7):
+        for c in range(r + 1, 7):
             binary_code.append(spec.matrix[r][c])
 
-    for i in range(1,6):
+    for i in range(1, 6):
         for idx in range(3):
-            if su.ALLOWED_OPS[idx] == spec.ops[i]:
+            if constants.ALLOWED_OPS[idx] == spec.ops[i]:
                 ternary_code.append(idx)
                 break
 
-    binary_code.extend( ternary_to_binary(ternary_code) )
+    binary_code.extend(ternary_to_binary(ternary_code))
     return binary_code
 
+
 def decode(binary_code):
-    matrix = su.np.zeros((7,7))
-    ops = [su.INPUT]
+    matrix = np.zeros((7, 7))
+    ops = [constants.INPUT]
 
     idx = 0
     for r in range(7):
-        for c in range(r+1,7):
+        for c in range(r + 1, 7):
             matrix[r][c] = binary_code[idx]
-            idx+=1
+            idx += 1
 
     binary_code = binary_code[idx:]
-    ternary_code = binary_to_ternary( binary_code )
+    ternary_code = binary_to_ternary(binary_code)
 
     for idx in range(5):
-        ops.append(su.ALLOWED_OPS[ ternary_code[idx] ])
-    ops.append(su.OUTPUT)
+        ops.append(constants.ALLOWED_OPS[ternary_code[idx]])
+    ops.append(constants.OUTPUT)
 
-    model_spec = su.api.ModelSpec( matrix, ops)
+    model_spec = constants.api.ModelSpec(matrix, ops)
     return model_spec
+
 
 def parent_selection(population, tournament_size):
     population_size = len(population)
     selected = []
     for _ in range(2):
-        indexes= su.random.sample(range(population_size), tournament_size)
+        indexes = random.sample(range(population_size), tournament_size)
         pool = list(population[i] for i in indexes)
-        pool.sort(key = crowded_comparison_operator)
+        pool.sort(key=crowded_comparison_operator)
         selected.append(pool[0])
     return selected
 
-def crossover( parent1, parent2):
-    index = su.random.sample(range(1,len(parent1)),1)
+
+def crossover(parent1, parent2):
+    index = random.sample(range(1, len(parent1)), 1)
     offspring = parent1[:index]
     offspring.extend(parent2[index:])
     return offspring
 
+
 def mutation(offspring, mutation_rate):
-    index = su.random.sample(range(1,len(offspring)),1)
+    index = random.sample(range(1, len(offspring)), 1)
     # bitwise
-    offspring[index] = ( 1 + offspring[index] ) % 2
+    offspring[index] = (1 + offspring[index]) % 2
     return offspring
+
 
 def generate_offspring(population,
                        crossover_prob,
                        tournament_size,
                        mutation_rate):
-
     population_size = len(population)
     size = 0
     offspring_population = []
     while size < population_size:
         # binary_tournament_selection
-        parent1, parent2  = parent_selection(population, tournament_size)
+        parent1, parent2 = parent_selection(population, tournament_size)
 
         parent1 = encode(parent1)
         parent2 = encode(parent2)
 
         # crossover
         offspring = parent1
-        if su.random.random() < crossover_prob:
+        if random.random() < crossover_prob:
             offspring = crossover(parent1, parent2)
 
         # mutation
-        if su.random.random() < mutation_rate:
+        if random.random() < mutation_rate:
             offspring = mutation(offspring)
 
         offspring_spec = decode(offspring)
 
-        if su.nasbench.is_valid(offspring_spec):
+        if constants.nasbench.is_valid(offspring_spec):
             size += 1
-            data = su.nasbench.query(offspring_spec)
+            data = constants.nasbench.query(offspring_spec)
             elem = {'acc': data['validation_accuracy'], 'time': data['training_time'], 'spec': offspring_spec}
             offspring_population.append(elem)
 
     return offspring_population
 
-def dominate_operator(elem1, elem2):
-    objectives = ['acc','time']
-    comp = [1,-1]
 
-    dominate_count=[0,0]
+def dominate_operator(elem1, elem2):
+    objectives = ['acc', 'time']
+    comp = [1, -1]
+
+    dominate_count = [0, 0]
     idx = 0
 
     for obj in objectives:
@@ -159,6 +159,7 @@ def dominate_operator(elem1, elem2):
         return -1
     else:
         return 0
+
 
 def fast_non_dominated_sort(population):
     S = []
@@ -189,22 +190,23 @@ def fast_non_dominated_sort(population):
 
     rank = 1
     while len(sorted[rank]) != 0:
-        sorted[rank+1] = set()
+        sorted[rank + 1] = set()
 
         for pidx in sorted[rank]:
             for qidx in S[pidx]:
                 n[qidx] -= 1
                 if n[qidx] == 0:
                     population[qidx]['rank'] = rank
-                    sorted[rank+1].add(qidx)
+                    sorted[rank + 1].add(qidx)
         rank += 1
 
     return sorted
 
-#def crowding_distance_assignment():
-#def crowded_comparison_operator( elem1, elem2 ):
-    # elem is dictionary{'acc', 'time', 'spec', 'rank'}
-    # return -1: elem1 < elem2  /   1: elem1 < elem2
+
+# def crowding_distance_assignment():
+# def crowded_comparison_operator( elem1, elem2 ):
+# elem is dictionary{'acc', 'time', 'spec', 'rank'}
+# return -1: elem1 < elem2  /   1: elem1 < elem2
 
 def nsga2(answer_size=500,
           search_time=25000,
@@ -213,25 +215,20 @@ def nsga2(answer_size=500,
           tournament_size=10,
           mutation_rate=0.03):
 
-    su.nasbench.reset_budget_counters()
+    constants.nasbench.reset_budget_counters()
     # 2 objectives: validation, time
-    population = [] # element is { rank, validation accuracy , time, spec } dictionary
+    population = []  # element is { rank, validation accuracy , time, spec } dictionary
 
     # For the first population_size individuals, seed the population with randomly
     # generated cells.
     for _ in range(population_size):
         spec = random_spec()
-        data = su.nasbench.query(spec)
+        data = constants.nasbench.query(spec)
         elem = {'acc': data['validation_accuracy'], 'time': data['training_time'], 'spec': spec}
-        population.append( elem )
+        population.append(elem)
 
     # evolution
     for _ in range(search_time):
         population += generate_offspring(population, crossover_prob, tournament_size, mutation_rate)
         fast_non_dominated_sort()
         number = 0
-
-
-
-
-
